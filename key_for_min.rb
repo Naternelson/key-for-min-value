c0cdae0c94e0dce8e35b454ba83c05c69c32c020
Nate <7nater.nelson@gmail.com>
2021-01-16 16:33:55 -0700

    commit tree d4c0c035273abbacabdde08b18e7c6d81f50ef88

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    return_key = nil
+    name_hash.each do |key, value|
+        if return_key == nil then
+            return_key = key
+        else
+            if value < name_hash[return_key] 
+                return_key = key
+            end
+        end
+    end
+    return_key
 end
+
+ikea = {:chair => 85, :table => 25, :mattress => 450}
+puts key_for_min_value(ikea) 
